cdc4439a5133348b2cedb3027ed1d185cb129ab2
common1 <my.common.developer@gmail.com>
2025-01-30 22:34:18 +0100

    05 Write Your First Solidity Smart Contract | Complete Solidity Programming Language Course - Part 2

--- README.md
+++ README.md
@@ -3,5 +3,5 @@
 ## 05 Write Your First Solidity Smart Contract | Complete Solidity Programming Language Course
 https://www.youtube.com/watch?v=3Fr4UdCyQVE&list=PLWUCKsxdKl0oksYr6IG_wRsaSUySQC0ck&index=5
 
-Current: 6:23
+---
 
--- hello.sol
+++ hello.sol
@@ -9,8 +9,14 @@ contract MyFirstCOntract {
     string public hey;
     uint256 public no;
 
-    constructor(string memory _hey) {
+    // constructor(string memory _hey, uint _no) {
+    //     hey = _hey;
+    //     no = _no;
+    // }
+
+    function addInfo(string memory _hey, uint _no) public  {
         hey = _hey;
+        no = _no;
     }
 }
 
